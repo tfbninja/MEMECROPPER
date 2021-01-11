4d4008dc0d87077fac2351b196a194c8c28217fc
Tim Barber <tfbninja@users.noreply.github.com>
2021-01-11 16:48:56 -0700

    you'll still most likely have to make changes
    assuming you're using the default installation of python 3.7 and your "GitHub" folder is in your documents folder like mine is, you won't even have to change this file.

--- cropmemes.bat
+++ cropmemes.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Python37\python.exe" "C:\Database\MEMECROPPER\cropTheGoddamnMemes.py"
+@replace with your python 3 .exe location		@replace with your cropTheGoddamnMemes.py location
+"C:\Program Files\Python37\python.exe" "%UserProfile%\Documents\GitHub\MEMECROPPER\cropTheGoddamnMemes.py"
