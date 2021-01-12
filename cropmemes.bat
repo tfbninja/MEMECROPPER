106711001e5a1284ad815ef2a60c96fbb5146eb4
Tim Barber <tfbninja@users.noreply.github.com>
2021-01-11 22:34:00 -0700

    Delete cropmemes.bat
    This is generated for you by setup.bat, you don't want mine.

--- cropmemes.bat
+++
@@ -1,2 +0,0 @@
-@replace with your python 3 .exe location		@replace with your cropTheGoddamnMemes.py location
-"C:\Program Files\Python37\python.exe" "%UserProfile%\Documents\GitHub\MEMECROPPER\cropTheGoddamnMemes.py"
